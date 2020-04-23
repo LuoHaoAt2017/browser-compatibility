4153a44e8ef4bed4f7212fa17e1cc2d06884b6e5
LuoHao-RenRen <hluo5206@gmail.com>
2020-04-23 17:30:41 +0800

    feat: 借助于辅助元素来实现滚动条的隐藏

--- src/components/hide-scroll-bar/index.js
+++ src/components/hide-scroll-bar/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Modal, Button } from 'antd';
 import './index.scss';
 
 class HideScrollBar extends React.Component {
@@ -34,85 +33,87 @@ class HideScrollBar extends React.Component {
     render() {
         return (
             <div className='hide-scroll-bar'>
-                <ul className='card-list'>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                    <li className='card'>
-                        <p className='poetry'>
-                            <label className='title'>
-                                黄鹤楼送孟浩然之广陵
-                    </label>
-                            <span className='line'>故人西辞黄鹤楼</span>
-                            <span className='line'>烟花三月下扬州</span>
-                            <span className='line'>孤帆远影碧空尽</span>
-                            <span className='line'>唯见长江天际流</span>
-                        </p>
-                    </li>
-                </ul>
+                <div className='card-list-container'>
+                    <ul className='card-list'>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                        <li className='card'>
+                            <p className='poetry'>
+                                <label className='title'>
+                                    黄鹤楼送孟浩然之广陵
+                                </label>
+                                <span className='line'>故人西辞黄鹤楼</span>
+                                <span className='line'>烟花三月下扬州</span>
+                                <span className='line'>孤帆远影碧空尽</span>
+                                <span className='line'>唯见长江天际流</span>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
